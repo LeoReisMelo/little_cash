67219086d1dcf7c721c85d2ee8f66bb2782b5488
Leonardo Reis Melo <61852766+LeoReisMelo@users.noreply.github.com>
2024-09-26 20:07:23 -0300

    feat: Add serviceOrder, clients and products (In development)

--- little_cash_backend/src/app/modules/auth/dtos/login.dto.ts
+++ little_cash_backend/src/app/modules/auth/dtos/login.dto.ts
@@ -1,16 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 @InputType()
 export class LoginDto {
   @Field()
   @IsEmail()
-  @IsNotEmpty({ message: 'E-mail not provided' })
   email: string;
 
   @Field()
-  @IsString({ message: 'Password must be a string' })
-  @IsNotEmpty({ message: 'Password not provided' })
-  @MinLength(6, { message: 'Password must have a minimum of 6 characters' })
+  @IsString()
   password: string;
 }
--- little_cash_backend/src/main.ts
+++ little_cash_backend/src/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      disableErrorMessages: false, // Desabilita mensagens de erro padrão
-    }),
-  );
 
   app.enableCors({
     origin: 'http://localhost:3000',
